0ed3f6cad46a0e2f18edef59ae4115b8761e4309
SakaiTaka23 <tt3.sakai@gmail.com>
2020-12-20 16:41:26 +0900

    ブックリストを作ってみた

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <Person/>
-      <Message/>
-    </div>
+    <section>
+      <Book />
+    </section>
   );
 }
 
-const Person = () => <h2>john doe</h2>;
+const Book = () => {
+  return (
+    <article>
+      <Image/>
+      <Title></Title>
+      <Author></Author>
+    </article>
+  );
+};
 
-const Message = () =>
+const Image = () => {
+  return (
+    <img
+      src='https://m.media-amazon.com/images/I/71wudfMLJML._AC_UL320_.jpg'
+      alt=''
+    />
+  );
+};
+
+const Title = () =>
 {
-  return <p>this is my message</p>;
+  return <h1>React.js & Next.js超入門</h1>;
+};
+
+const Author = () =>
+{
+  return <h4>掌田津耶乃</h4>;
 }
 
-
-ReactDom.render(<Greeting />, document.getElementById('root'));
+ReactDom.render(<BookList />, document.getElementById('root'));
