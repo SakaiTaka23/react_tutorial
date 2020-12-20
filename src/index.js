c8b8af8b4383cdb3b305e03c0d235c6bad5e3c63
SakaiTaka23 <tt3.sakai@gmail.com>
2020-12-20 16:25:04 +0900

    入れ子の構造について学習

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 function Greeting() {
-  return <div><h4>this is john and this is my first component</h4></div>;
+  return (
+    <div>
+      <Person/>
+      <Message/>
+    </div>
+  );
 }
 
-// const Greeting = () => {
-//   return React.createElement(
-//     'div',
-//     {},
-//     React.createElement('h1', {}, 'hello world')
-//   );
-// };
+const Person = () => <h2>john doe</h2>;
+
+const Message = () =>
+{
+  return <p>this is my message</p>;
+}
+
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
