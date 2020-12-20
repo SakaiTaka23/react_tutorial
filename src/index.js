206e0d05191b8f020e0e5a2c0318c16ee71bae3f
SakaiTaka23 <tt3.sakai@gmail.com>
2020-12-20 18:05:51 +0900

    propsについて学習

--- src/index.js
+++ src/index.js
@@ -4,48 +4,37 @@ import ReactDom from 'react-dom';
 // CSS
 import './index.css';
 
+const firstBook = {
+  img: 'https://m.media-amazon.com/images/I/71wudfMLJML._AC_UL320_.jpg',
+  title: 'React.js & Next.js超入門',
+  author: '掌田津耶乃',
+};
+
+const secondBook = {
+  img: 'https://m.media-amazon.com/images/I/71-wOEcxOKL._AC_UL320_.jpg',
+  title: 'React開発 現場の教科書',
+  author: '石橋 啓太 ',
+};
+
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
+        <p>reactに関する本</p>
+      </Book>
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
     </section>
   );
 }
 
-const Book = () => {
+const Book = ({img,title,author,children}) => {
   return (
     <article className='book'>
-      <Image />
-      <Title></Title>
-      <Author></Author>
+      <img src={img} alt='' />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {children}
     </article>
-  );
-};
-
-const Image = () => {
-  return (
-    <img
-      src='https://m.media-amazon.com/images/I/71wudfMLJML._AC_UL320_.jpg'
-      alt=''
-    />
-  );
-};
-
-const Title = () => {
-  return <h1>React.js & Next.js超入門</h1>;
-};
-
-const Author = () => {
-  return (
-    <h4 style={{ color: 'red', fontSize: '0.75rem', marginTop: '0.25rem' }}>
-      掌田津耶乃
-    </h4>
   );
 };
 
