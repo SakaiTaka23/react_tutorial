8fc86f566107ad095ea14f1984e16da41444a775
SakaiTaka23 <tt3.sakai@gmail.com>
2020-12-23 10:22:41 +0900

    listについて学習

--- src/index.js
+++ src/index.js
@@ -4,36 +4,39 @@ import ReactDom from 'react-dom';
 // CSS
 import './index.css';
 
-const firstBook = {
-  img: 'https://m.media-amazon.com/images/I/71wudfMLJML._AC_UL320_.jpg',
-  title: 'React.js & Next.js超入門',
-  author: '掌田津耶乃',
-};
-
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/I/71-wOEcxOKL._AC_UL320_.jpg',
-  title: 'React開発 現場の教科書',
-  author: '石橋 啓太 ',
-};
+const books = [
+  {
+    id:1,
+    img: 'https://m.media-amazon.com/images/I/71wudfMLJML._AC_UL320_.jpg',
+    title: 'React.js & Next.js超入門',
+    author: '掌田津耶乃',
+  },
+  {
+    id:2,
+    img: 'https://m.media-amazon.com/images/I/71-wOEcxOKL._AC_UL320_.jpg',
+    title: 'React開発 現場の教科書',
+    author: '石橋 啓太 ',
+  },
+];
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
-        <p>reactに関する本</p>
-      </Book>
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
+      {books.map((book) => {
+        // const { img, title, author } = book;
+        return <Book key={book.id} book={book}></Book>;
+      })}
     </section>
   );
 }
 
-const Book = ({img,title,author,children}) => {
+const Book = (props) => {
+  const { img, title, author } = props.book;
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
