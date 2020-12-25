9ab08d21d29b96303b6f0f1ec327367ec80b3a8d
SakaiTaka23 <tt3.sakai@gmail.com>
2020-12-25 17:34:28 +0900

    accordionアプリ作成

--- react-projects/4-accordion/setup/src/App.js
+++ react-projects/4-accordion/setup/src/App.js
@@ -1,8 +1,23 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  console.log(data);
+  const [questions, setQuestions] = useState(data);
+
+  return (
+    <>
+      <div className='container'>
+        <h3>Questions And Answers About Login</h3>
+        <section className='info'>
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </>
+  );
 }
 
 export default App;
