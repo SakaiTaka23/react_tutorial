fd464db93a8b4176649ced259bb5ef234a2113b0
SakaiTaka23 <tt3.sakai@gmail.com>
2020-12-28 17:19:01 +0900

    groceryアプリu3000追加できるようにした

--- react-projects/10-grocery-bud/setup/src/App.js
+++ react-projects/10-grocery-bud/setup/src/App.js
@@ -1,9 +1,59 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({
+    show: true,
+    msg: 'hi',
+    type: '',
+    type: 'danger',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      //display alert
+    } else if (name && isEditing) {
+      //edit
+    } else {
+      // create
+      const newItem = { id: new Date().getTime.toString(), title: name };
+      setList([...list, newItem]);
+      setName('');
+    }
+  };
+
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} />}
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='eg. eggs'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} />
+          <button className='clear-btn'>clear items</button>
+        </div>
+      )}
+    </section>
+  );
 }
 
-export default App
+export default App;
