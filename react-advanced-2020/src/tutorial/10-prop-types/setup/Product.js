135786dd862c85b5c2ae564d0cc4cf74113865b6
SakaiTaka23 <tt3.sakai@gmail.com>
2020-12-29 18:26:12 +0900

    prop typesについて学習

--- react-advanced-2020/src/tutorial/10-prop-types/setup/Product.js
+++ react-advanced-2020/src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import defaultImage from '../../../assets/default-image.jpeg';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ image, name, price }) => {
+  const url = image && image.url;
+  
+  return (
+    <article className='product'>
+      <img src={url || defaultImage} alt={name} />
+      <h4>{name}</h4>
+      <p>${price || 3.99}</p>
+    </article>
+  );
 };
 
 export default Product;
