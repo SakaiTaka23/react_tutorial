b69752e205ffdb9acc821fcb6c2451b8156de4c1
SakaiTaka23 <tt3.sakai@gmail.com>
2020-12-29 10:45:57 +0900

    useReducer アイテムの追加について学習

--- react-advanced-2020/src/tutorial/6-useReducer/setup/index.js
+++ react-advanced-2020/src/tutorial/6-useReducer/setup/index.js
@@ -3,11 +3,32 @@ import Modal from './Modal';
 import { data } from '../../../data';
 
 // reducer function
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  if (action.type === 'ADD_ITEM') {
+    const newPeople = [...state.people, action.payload];
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: 'item added',
+    };
+  }
+  if (action.type === 'NO_VALUE') {
+    return {
+      ...state,
+      isModalOpen: true,
+      modalContent: 'please enter value',
+    };
+  }
+  if (action.type === 'CLOSE_MODAL') {
+    return { ...state, isModalOpen: false };
+  }
+  throw new Error('no matching action type');
+};
 const defaultState = {
-  people: data,
-  isModalOpen: true,
-  modalContent: 'hello world',
+  people: [],
+  isModalOpen: false,
+  modalContent: '',
 };
 
 const Index = () => {
@@ -17,13 +38,21 @@ const Index = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: 'ADD_ITEM', payload: newItem });
+      setName('');
     } else {
+      dispatch({ type: 'NO_VALUE' });
     }
+  };
+
+  const closeModal = () => {
+    dispatch({ type: 'CLOSE_MODAL' });
   };
 
   return (
     <>
-      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      {state.isModalOpen && <Modal closeModal={closeModal} modalContent={state.modalContent} />}
       <form onSubmit={handleSubmit} className='form'>
         <div>
           <input
