d3d4358b407a13c3d274e19c6f737e8df9e42446
SakaiTaka23 <tt3.sakai@gmail.com>
2020-12-30 17:23:26 +0900

    cocktails 関数最適化、微調整

--- react-projects/15-cocktails/setup/src/components/SearchForm.js
+++ react-projects/15-cocktails/setup/src/components/SearchForm.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useRef } from 'react';
 import { useGlobalContext } from '../context';
 
 const SearchForm = () => {
@@ -10,9 +9,17 @@ const SearchForm = () => {
     setSearchTerm(searchValue.current.value);
   };
 
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <section className='section search'>
-      <form className='search-form'>
+      <form className='search-form' onSubmit={handleSubmit}>
         <div className='form-control'>
           <label htmlFor='name'>search your favorite cocktail</label>
           <input
