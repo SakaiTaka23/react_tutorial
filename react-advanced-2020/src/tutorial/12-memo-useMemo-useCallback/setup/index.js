6322985590122ec8fce377bebaa263e5e80144b9
SakaiTaka23 <tt3.sakai@gmail.com>
2020-12-30 10:16:50 +0900

    memo,useCallbackについて学習

--- react-advanced-2020/src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ react-advanced-2020/src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -1,15 +1,20 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react'
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import { useFetch } from '../../9-custom-hooks/final/2-useFetch';
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/javascript-store-products'
+const url = 'https://course-api.com/javascript-store-products';
 
 // every time props or state changes, component re-renders
 
 const Index = () => {
-  const { products } = useFetch(url)
-  const [count, setCount] = useState(0)
+  const { products } = useFetch(url);
+  const [count, setCount] = useState(0);
+  const [cart, setCart] = useState(0);
+
+  const addToCart = useCallback(() => {
+    setCart(cart + 1);
+  },[cart]);
 
   return (
     <>
@@ -17,32 +22,42 @@ const Index = () => {
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{ marginTop: '3rem' }}>cart : {cart}</h1>
+      <BigList products={products} addToCart={addToCart} />
     </>
-  )
-}
+  );
+};
 
-const BigList = ({ products }) => {
+const BigList = React.memo(({ products, addToCart }) => {
+  console.log('big list');
   return (
     <section className='products'>
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return (
+          <SingleProduct
+            key={product.id}
+            {...product}
+            addToCart={addToCart}
+          ></SingleProduct>
+        );
       })}
     </section>
-  )
-}
+  );
+});
 
-const SingleProduct = ({ fields }) => {
-  let { name, price } = fields
-  price = price / 100
-  const image = fields.image[0].url
+const SingleProduct = ({ fields, addToCart }) => {
+  console.count('single product');
+  let { name, price } = fields;
+  price = price / 100;
+  const image = fields.image[0].url;
 
   return (
     <article className='product'>
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>add to cart</button>
     </article>
-  )
-}
-export default Index
+  );
+};
+export default Index;
