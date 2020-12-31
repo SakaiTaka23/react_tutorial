612d9ec0815306c472e30a75db2656ee435c3cfe
SakaiTaka23 <tt3.sakai@gmail.com>
2020-12-31 11:08:11 +0900

    object,arrayについて学習

--- typescript_tutorial/app.js
+++ typescript_tutorial/app.js
@@ -1,14 +1,11 @@
-console.log('hello');
-function add(n1, n2, showResult, resultPhrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(resultPhrase + result);
-    }
-    return result;
+var person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+};
+var favoriteActivities;
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-var result = add(number1, number2, printResult, resultPhrase);
-console.log(result);
--- typescript_tutorial/app.ts
+++ typescript_tutorial/app.ts
@@ -1,17 +1,13 @@
-console.log('hello');
+const person = {
+  name: 'Maximilian',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+};
 
-function add(n1: number, n2: number, showResult: boolean, resultPhrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(resultPhrase+result);
-  }
-  return result;
+let favoriteActivities: string[];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
 }
-
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-const result = add(number1, number2, printResult,resultPhrase);
-console.log(result);
