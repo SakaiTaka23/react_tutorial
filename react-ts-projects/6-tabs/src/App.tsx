62f855952f75e654347a9fa57a9a67dac4a67119
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-15 17:23:53 +0900

    6 必要なコンポーネント作成

--- react-ts-projects/6-tabs/src/App.tsx
+++ react-ts-projects/6-tabs/src/App.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Loading from './Components/Loading';
+import Tab from './Components/Tab';
+
+const url = 'https://course-api.com/react-tabs-project';
 
 function App() {
-  return <div className='App'>Expierence</div>;
+  const [jobs, setJobs] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading === true) {
+    return <Loading />;
+  }
+
+  return <Tab />;
 }
 
 export default App;
---
+++ react-ts-projects/6-tabs/src/Components/Tab.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Tab = () => {
+  return <div>Tab Component</div>;
+};
+
+export default Tab;
