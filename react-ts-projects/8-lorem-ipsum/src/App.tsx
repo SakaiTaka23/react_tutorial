c6b337912657e7017300564e5f4c0f1505127aa8
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-16 12:04:47 +0900

    8 必要なコンポーネント作成

--- react-ts-projects/8-lorem-ipsum/src/App.tsx
+++ react-ts-projects/8-lorem-ipsum/src/App.tsx
@@ -1,8 +1,17 @@
+import { Typography } from '@material-ui/core';
 import React from 'react';
 import './App.css';
+import Form from './Components/Form';
+import Lorem from './Components/Lorem';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Typography>tired of boring lorem ipsum?</Typography>
+      <Form />
+      <Lorem />
+    </div>
+  );
 }
 
 export default App;
---
+++ react-ts-projects/8-lorem-ipsum/src/Components/Lorem.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Lorem = () => {
+  return <div>Lorem Component</div>;
+};
+
+export default Lorem;
