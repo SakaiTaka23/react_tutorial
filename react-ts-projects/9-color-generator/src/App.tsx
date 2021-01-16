29179b811d50f0b0f4cf1a1b5ae20c41d2055351
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-16 15:00:07 +0900

    9 必要なコンポーネント準備

--- react-ts-projects/9-color-generator/src/App.tsx
+++ react-ts-projects/9-color-generator/src/App.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import randomColor from 'randomcolor';
 import './App.css';
+import { Box, Typography } from '@material-ui/core';
+import Form from './Components/Form';
+import SingleColor from './Components/SingleColor';
 
 function App() {
-  return <div className='App'></div>;
+  const green = randomColor({
+    count: 10,
+    hue: 'green',
+  });
+  console.log(green);
+
+  return (
+    <div className='App'>
+      <Box display='flex' flexDirection='row'>
+        <Box mx={5}>
+          <Typography variant='h5'>Color Generator</Typography>
+        </Box>
+        <Box>
+          <Form />
+        </Box>
+      </Box>
+      <Box display='flex' flexWrap='wrap'>
+        <SingleColor />
+      </Box>
+    </div>
+  );
 }
 
 export default App;
