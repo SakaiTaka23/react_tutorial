ea1166970a91002ccbf3eb5c6adaab4fa048e601
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-17 15:57:23 +0900

    todo-context todoを作ってみた

--- react-ts-projects/todo-context/src/App.tsx
+++ react-ts-projects/todo-context/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react';
+import TodosProvider, { TodosContext } from './TodosContext';
 
-function App() {
+const App = () => {
+  const { todos, addTodo } = useContext(TodosContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {todos.map((todo, i) => (
+          <div key={i}>{todo}</div>
+        ))}
+      </div>
+      <button onClick={() => addTodo('new todo')}>add todo</button>
     </div>
   );
-}
+};
 
-export default App;
+export default () => (
+  <TodosProvider>
+    <App />
+  </TodosProvider>
+);
