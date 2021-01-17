1241deb8d35f987e27fbf4d1a5221d8f80c07d03
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-17 10:14:48 +0900

    10 必要なコンポーネント準備

--- react-ts-projects/10-grocery-bud/src/App.tsx
+++ react-ts-projects/10-grocery-bud/src/App.tsx
@@ -1,8 +1,23 @@
+import { Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import './App.css';
+import Alert from './Components/Alert';
+import Clear from './Components/Clear';
+import Form from './Components/Form';
+import List from './Components/List';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <Grid container justify='center' alignItems='center'>
+      <Grid item className='glass' xs={6}>
+        <Alert />
+        <Typography variant='h5'>Grocery Bud</Typography>
+        <Form />
+        <List />
+        <Clear />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default App;
