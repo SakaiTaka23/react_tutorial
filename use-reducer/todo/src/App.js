f9b0a8b7b04f49b12219b7cccbe29096bb11ad6b
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-25 11:05:40 +0900

    reducerを使って簡単なtodo作成

--- use-reducer/todo/src/App.js
+++ use-reducer/todo/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
+const ACTIONS = {
+  ADD_TODO: 'add_todo',
+};
+
+const reducer = (todos, action) => {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)];
+
+    default:
+      break;
+  }
+};
+
+const newTodo = (name) => {
+  return { id: Date.now(), name: name, complete: false };
+};
+
 function App() {
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+    setName('');
+  };
+
+  console.log(todos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
+      </form>
+    </>
   );
 }
 
