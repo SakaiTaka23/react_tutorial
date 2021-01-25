48f1435bf6751ef8fbc65f27f3bda98ae59e5a36
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-25 16:45:43 +0900

    簡単なtodo作成

--- use-reducer/todo/src/App.js
+++ use-reducer/todo/src/App.js
@@ -1,17 +1,28 @@
 import React, { useReducer, useState } from 'react';
 import './App.css';
+import Todo from './Todo';
 
-const ACTIONS = {
+export const ACTIONS = {
   ADD_TODO: 'add_todo',
+  TOGGLE_TODO: 'toggle_todo',
+  DELETE_TODO: 'delete_todo,',
 };
 
 const reducer = (todos, action) => {
   switch (action.type) {
     case ACTIONS.ADD_TODO:
       return [...todos, newTodo(action.payload.name)];
-
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete };
+        }
+        return todo;
+      });
+    case ACTIONS.DELETE_TODO:
+      return todos.filter((todo) => todo.id !== action.payload.id);
     default:
-      break;
+      return todos;
   }
 };
 
@@ -35,6 +46,9 @@ function App() {
       <form onSubmit={handleSubmit}>
         <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
       </form>
+      {todos.map((todo) => {
+        return <Todo key={todo.id} todo={todo} dispatch={dispatch} />;
+      })}
     </>
   );
 }
