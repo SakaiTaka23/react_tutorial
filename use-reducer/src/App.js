f54269ddd556f13f2be03ee4a4309a8ff46f569a
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-25 10:18:46 +0900

    足し算をするようなアプリを作成

--- use-reducer/src/App.js
+++ use-reducer/src/App.js
@@ -1,7 +1,26 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className='App'>
+      <>
+        <button onClick={() => decrement()}>-</button>
+        <span>{count}</span>
+        <button onClick={() => increment()}>+</button>
+      </>
+    </div>
+  );
 }
 
 export default App;
