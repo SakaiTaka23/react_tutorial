39cff0558b2d42d9b109f0bb4470316dbfd79e94
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-26 13:36:36 +0900

    14 合計金額を表示

--- react-ts-projects/14-cart/src/Components/Total.tsx
+++ react-ts-projects/14-cart/src/Components/Total.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from '../CartContext';
 
 const Total = () => {
-  return <div>total</div>;
+  const { cartItem } = useContext(CartContext);
+  const sum = cartItem.items.reduce((prev_sum, item) => prev_sum + item.price * item.amount, 0);
+
+  return <div>total : {sum}</div>;
 };
 
 export default Total;
