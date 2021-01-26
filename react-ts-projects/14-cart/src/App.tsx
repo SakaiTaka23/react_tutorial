7c944834cbe2ae109ca11455f3d63ec2111c7925
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-26 14:46:35 +0900

    14 商品が0個の時の処理ついか

--- react-ts-projects/14-cart/src/App.tsx
+++ react-ts-projects/14-cart/src/App.tsx
@@ -4,17 +4,30 @@ import Nav from './Components/Nav';
 import ItemList from './Components/ItemList';
 import Total from './Components/Total';
 import Clear from './Components/Clear';
+import { CartContext } from './CartContext';
 
 function App() {
-  return (
-    <>
-      <Nav />
-      YOUR BAG
-      <ItemList />
-      <Total />
-      <Clear />
-    </>
-  );
+  const { cartItem } = useContext(CartContext);
+  const count = cartItem.items.length;
+
+  if (count > 0) {
+    return (
+      <>
+        <Nav />
+        YOUR BAG
+        <ItemList />
+        <Total />
+        <Clear />
+      </>
+    );
+  } else {
+    return (
+      <div>
+        <Nav />
+        YOUR BAG IS EMPTY
+      </div>
+    );
+  }
 }
 
 export default App;
