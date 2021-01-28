9dbb498e633b75aed8d734fac6e2a346597df0ef
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-28 13:40:15 +0900

    15 必要になりそうなルート準備

--- react-ts-projects/15-cocktails/src/App.tsx
+++ react-ts-projects/15-cocktails/src/App.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar';
+import About from './Pages/About';
+import Error from './Pages/Error';
+import Home from './Pages/Home';
+import SingleCocktail from './Pages/SingleCocktail';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/cocktail/:id'>
+          <SingleCocktail />
+        </Route>
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ react-ts-projects/15-cocktails/src/Pages/SingleCocktail.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SingleCocktail = () => {
+  return <div>singele cocktail</div>;
+};
+
+export default SingleCocktail;
