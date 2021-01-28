1a17a5e2bfcb2a995bad0f320a006ed883a5b048
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-28 18:04:43 +0900

    15 カクテルの取得を最適化

--- react-ts-projects/15-cocktails/src/CocktailContext.tsx
+++ react-ts-projects/15-cocktails/src/CocktailContext.tsx
@@ -1,24 +1,35 @@
 import { createContext, FC, useEffect, useState } from 'react';
+import { cocktail } from './Types/cocktailType';
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 type CocktailContextState = {
   search: string;
   setSearch: React.Dispatch<React.SetStateAction<string>>;
-  cocktails: any;
+  cocktails: cocktail[];
 };
 
 const CocktailContext = createContext({} as CocktailContextState);
 
 const CocktailProvider: FC = ({ children }) => {
   const [search, setSearch] = useState('');
-  const [cocktails, setCocktails] = useState({});
+  const [cocktails, setCocktails] = useState<cocktail[]>([]);
 
   console.log(cocktails);
 
   const fetchCocktails = async (search: string) => {
-    const response = await fetch(url + search);
-    const newCocktails = await response.json();
+    const response = await (await fetch(url + search)).json();
+    const { drinks } = response;
+    const newCocktails: cocktail[] = drinks.map((drink: any) => {
+      const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = drink;
+      return {
+        id: idDrink,
+        name: strDrink,
+        image: strDrinkThumb,
+        info: strAlcoholic,
+        glass: strGlass,
+      };
+    });
     setCocktails(newCocktails);
   };
 
---
+++ react-ts-projects/15-cocktails/src/Types/cocktailType.ts
@@ -0,0 +1,9 @@
+type cocktail = {
+  id: string;
+  name: string;
+  image: string;
+  info: string;
+  glass: string;
+};
+
+export type { cocktail };
