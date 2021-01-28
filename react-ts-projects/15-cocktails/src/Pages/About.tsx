7d5e7a302e66f4f8aa4fa582e641599ab6ce40eb
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-28 13:48:49 +0900

    15 aboutページ完成

--- react-ts-projects/15-cocktails/src/Pages/About.tsx
+++ react-ts-projects/15-cocktails/src/Pages/About.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const About = () => {
-  return <div>about</div>;
+  return (
+    <>
+      <div>About us</div>
+      <div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim omnis molestiae ratione fugit sed quis amet sequi
+        at porro odio nihil iusto impedit obcaecati voluptatibus possimus, voluptatum non placeat! Molestiae! Lorem,
+        ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsum aspernatur alias nulla sint eos veritatis.
+        Maxime delectus iure velit pariatur consequuntur ullam eos eveniet voluptate dolores sed, animi ex.
+      </div>
+    </>
+  );
 };
 
 export default About;
