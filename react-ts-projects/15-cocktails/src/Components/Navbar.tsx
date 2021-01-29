c87133ed49688bcbab478d582979bfaa740a1bb9
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-29 11:56:10 +0900

    15 nav作成

--- react-ts-projects/15-cocktails/src/Components/Navbar.tsx
+++ react-ts-projects/15-cocktails/src/Components/Navbar.tsx
@@ -1,13 +1,25 @@
+import { AppBar, Box, Button, Typography } from '@material-ui/core';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useStyles } from '../Theme';
 
 const Navbar = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      nav bar
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
-    </div>
+    <AppBar className={classes.glass} position='static'>
+      <Box display='flex' flexDirection='row' p={1} mx={3}>
+        <Box flexGrow={1}>
+          <Typography variant='h4'>TheCocktailDB</Typography>
+        </Box>
+        <Box>
+          <Button>
+            <Link to='/'>Home</Link>
+          </Button>
+          <Link to='/about'>About</Link>
+        </Box>
+      </Box>
+    </AppBar>
   );
 };
 
