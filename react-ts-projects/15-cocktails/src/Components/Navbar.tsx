85017999487d287fbd262422925177145deeeaa4
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-29 09:46:52 +0900

    15 navのリンク作成

--- react-ts-projects/15-cocktails/src/Components/Navbar.tsx
+++ react-ts-projects/15-cocktails/src/Components/Navbar.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  return <div>nav bar</div>;
+  return (
+    <div>
+      nav bar
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+    </div>
+  );
 };
 
 export default Navbar;
