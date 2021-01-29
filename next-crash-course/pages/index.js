f31076e81f49305016c3bbf20a0d03a9b57e28a6
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-29 16:09:10 +0900

    ルーティングをしてみた、コンポーネントを作ってみた

--- next-crash-course/pages/index.js
+++ next-crash-course/pages/index.js
@@ -3,6 +3,11 @@ import Head from 'next/head';
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>web dev</title>
+        <meta name='key' />
+      </Head>
+
       <h1>welcome to next</h1>
     </div>
   );
