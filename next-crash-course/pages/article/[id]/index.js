8e38dee58ae22e94cb593feb26850011e76291f4
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-29 17:14:46 +0900

    サーバー再度で引数を得る方法を学習

--- next-crash-course/pages/article/[id]/index.js
+++ next-crash-course/pages/article/[id]/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
-const index = () => {
-  const router = useRouter();
-  const { id } = router.query;
+const index = ({ article }) => {
+  //   const router = useRouter();
+  //   const { id } = router.query;
 
-  return <div>This is an article {id}</div>;
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href='/'>Go Back</Link>
+    </>
+  );
+};
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
 };
 
 export default index;
