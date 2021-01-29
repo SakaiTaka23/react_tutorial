f7baa6fb37ad2b96ca18c6007b4dbb13b3dc0a50
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-29 13:09:20 +0900

    15 formのデザイン実装

--- react-ts-projects/15-cocktails/src/Components/SearchForm.tsx
+++ react-ts-projects/15-cocktails/src/Components/SearchForm.tsx
@@ -1,20 +1,30 @@
+import { Box, Paper, TextField, Typography } from '@material-ui/core';
 import React, { useContext } from 'react';
 import { CocktailContext } from '../CocktailContext';
+import { useStyles } from '../Theme';
 
 const SearchForm = () => {
+  const classes = useStyles();
   const { search, setSearch } = useContext(CocktailContext);
   console.log(search);
 
   return (
-    <form>
-      <input
-        type='text'
-        value={search}
-        onChange={(e) => {
-          setSearch(e.target.value);
-        }}
-      />
-    </form>
+    <Box p={5} display='flex' alignContent='center' justifyContent='center'>
+      <Paper className={`${classes.glass} form`}>
+        <Typography variant='h5'>Search Your Favorite Cocktail</Typography>
+        <form>
+          <TextField
+            autoFocus
+            fullWidth
+            type='text'
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          />
+        </form>
+      </Paper>
+    </Box>
   );
 };
 
