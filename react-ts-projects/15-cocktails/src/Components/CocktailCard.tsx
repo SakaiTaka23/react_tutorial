0a340102eb5c3ab996a9a2440d3c86d25d247ad5
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-29 13:53:06 +0900

    15 card,card listのデザインをした

--- react-ts-projects/15-cocktails/src/Components/CocktailCard.tsx
+++ react-ts-projects/15-cocktails/src/Components/CocktailCard.tsx
@@ -1,16 +1,28 @@
+import { Card, CardActionArea, CardActions, CardContent, Typography } from '@material-ui/core';
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
+import { useStyles } from '../Theme';
 import { cocktail } from '../Types/cocktailType';
 
 const CocktailCard: FC<cocktail> = ({ id, name, image, info, glass }) => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <img src={image} alt={name} />
-      <div>{name}</div>
-      <div>{glass}</div>
-      <p>{info}</p>
-      <Link to={`/cocktail/${id}`}>detail</Link>
-    </div>
+    <Card className={classes.glass}>
+      <CardActionArea>
+        <img src={image} alt={name} />
+        <CardContent>
+          <Typography variant='h5'>{name}</Typography>
+          <Typography variant='subtitle1'>{glass}</Typography>
+          <Typography variant='body1' paragraph>
+            {info}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Link to={`/cocktail/${id}`}>DETAILS</Link>
+      </CardActions>
+    </Card>
   );
 };
 
--- react-ts-projects/15-cocktails/src/Components/CocktailList.tsx
+++ react-ts-projects/15-cocktails/src/Components/CocktailList.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@material-ui/core';
 import React, { useContext } from 'react';
 import { CocktailContext } from '../CocktailContext';
 import CocktailCard from './CocktailCard';
@@ -6,11 +7,15 @@ const CocktailList = () => {
   const { cocktails } = useContext(CocktailContext);
 
   return (
-    <>
+    <Box display='flex' flexWrap='wrap' justifyContent='center' alignItems='center' m={2}>
       {cocktails.map((cocktail) => {
-        return <CocktailCard key={cocktail.id} {...cocktail} />;
+        return (
+          <Box m={2} key={cocktail.id}>
+            <CocktailCard {...cocktail} />
+          </Box>
+        );
       })}
-    </>
+    </Box>
   );
 };
 
