ce990a959f07ebc08da31a6ed8546f189a2551da
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-29 11:55:59 +0900

    15 theme作成

--- react-ts-projects/15-cocktails/src/App.tsx
+++ react-ts-projects/15-cocktails/src/App.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
@@ -7,25 +8,28 @@ import About from './Pages/About';
 import Error from './Pages/Error';
 import Home from './Pages/Home';
 import SingleCocktail from './Pages/SingleCocktail';
+import { theme } from './Theme';
 
 function App() {
   return (
     <CocktailProvider>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/about'>
-            <About />
-          </Route>
-          <Route path='/cocktail/:id' component={SingleCocktail}></Route>
-          <Route path='*'>
-            <Error />
-          </Route>
-        </Switch>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/about'>
+              <About />
+            </Route>
+            <Route path='/cocktail/:id' component={SingleCocktail}></Route>
+            <Route path='*'>
+              <Error />
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
     </CocktailProvider>
   );
 }
