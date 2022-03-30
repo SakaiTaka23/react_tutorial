eecdd43d1fc28aceb3efe2630ea3b5755f27ac51
SakaiTaka23 <tt3.sakai@gmail.com>
2022-03-30 21:27:55 +0900

    メタモンの情報をとってみた

---
+++ next-suspense/components/Loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Loading = () => {
+    console.log('loading')
+    return (
+    <div>Loading...</div>
+  )
+}
+
+export default Loading
--- next-suspense/pages/_app.tsx
+++ next-suspense/pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Suspense } from 'react'
+import Loading from '../components/Loading'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Suspense fallback={<Loading/>}>
+    <Component {...pageProps} />
+    </Suspense>
 }
 
 export default MyApp
